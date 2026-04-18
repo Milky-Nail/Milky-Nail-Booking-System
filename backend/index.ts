17a2235ebdd94553e0fd7bbf10a615cf503502a7
Ryan Kuo <t5a2r0@gmail.com>
2026-04-18 22:49:44 +0800

    chore: 準備部署到neon上

--- backend/index.ts
+++ backend/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // TODO:正式上線前要改成前端網址
+    origin: ["http://localhost:5173", "https://milky-nail-test.onrender.com"], // TODO:正式上線前要改成前端網址
     credentials: true,
   })
 );
