5f10e260c9012543eef06a5a0374fb40330a68e2
Ryan <t5a2r0@gmail.com>
2026-05-23 21:05:49 +0800

    chore: 增加helmet套件安全標頭

--- backend/index.ts
+++ backend/index.ts
@@ -9,6 +9,7 @@ import rootRouter from "./src/routes/index";
 import passport from "./src/config/passport";
 import path from "path";
 import { fileURLToPath } from "url";
+import helmet from "helmet";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +19,7 @@ const __dirname = path.dirname(__filename);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", `${process.env.FRONTEND_URL}`],
+    origin: [`${process.env.FRONTEND_URL}`],
     credentials: true,
   })
 );
@@ -26,6 +27,16 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/api", rootRouter);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "data:", "https:"],
+      "script-src": ["'self'", "'unsafe-inline'"],
+    },
+  })
+);
 app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
