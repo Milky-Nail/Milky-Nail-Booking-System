f3624e603a0aeda37326dd3a7d03b134dec12a5c
Ryan <t5a2r0@gmail.com>
2026-05-24 02:05:50 +0800

    fix: SEO調整

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import "material-symbols";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
@@ -22,7 +19,6 @@ library.add(faFacebook, faInstagram, faGoogle, faLine);
 const app = createApp(App);
 const pinia = createPinia();
 app.use(router);
-app.use(ElementPlus);
 app.use(pinia.use(piniaPluginPersistedstate));
 app.use(
   createGtm({
